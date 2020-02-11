65d4018afa62402106def6832f9851827f31e7b0
billdelvin <billdelvin6@gmail.com>
2020-02-11 19:42:48 +0700

    adding build control

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.4
+};
+
+class BurgerBuilder extends Component {
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     ...
+  //   }
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  };
+
+  addIngredientHandler = type => {
+    const oldIngredient = this.state.ingredients[type];
+    const updatedCount = oldIngredient + 1;
+    const updatedIngredient = {
+      ...this.state.ingredients
+    };
+    updatedIngredient[type] = updateupdatedCountCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredient
+    });
+  };
+
+  removeIngredientHandler = type => {};
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
