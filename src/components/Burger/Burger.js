0e20fb295a7894d596919a773664ad7d64dffd97
billdelvin <billdelvin6@gmail.com>
2020-02-11 19:40:01 +0700

    adding burger.js

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  let transformIngredients = Object.keys(props.ingredients)
+    .map(igItem => {
+      return [...Array(props.ingredients[igItem])].map((_, i) => {
+        return <BurgerIngredient key={igItem + i} type={igItem} />;
+      });
+    })
+    .reduce((prevVal, currVal) => {
+      return prevVal.concat(currVal);
+    }, []);
+
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredients!</p>
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {/* <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" /> */}
+      {transformIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
