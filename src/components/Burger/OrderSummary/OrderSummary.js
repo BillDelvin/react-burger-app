ea5aab40df77e018ded93b16de3960855ec51ee4
billdelvin <billdelvin6@gmail.com>
2020-02-12 19:17:19 +0700

    adding button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Aux";
+import Button from "../../../components/UI/Button/Button";
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igItem => {
