9364d95607e0196a2a79a9f44152305c7be4ed10
billdelvin <billdelvin6@gmail.com>
2020-02-12 18:15:12 +0700

    adding build contorl

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,9 +12,27 @@ const controls = [
 const buildControls = props => {
   return (
     <div className={classes.BuildControls}>
+      <p>
+        Current Price : <strong>{props.price.toFixed(2)}</strong>
+      </p>
       {controls.map(ctrl => {
-        return <BuildControl key={ctrl.label} label={ctrl.label} added={props.ingredientAdded}/>;
+        return (
+          <BuildControl
+            key={ctrl.label}
+            label={ctrl.label}
+            added={() => props.ingredientAdded(ctrl.type)}
+            remove={() => props.ingredientRemove(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}
+          />
+        );
       })}
+      <button
+        className={classes.OrderButton}
+        disabled={!props.purchaseable}
+        onClick={props.ordered}
+      >
+        ORDER NOW
+      </button>
     </div>
   );
 };
