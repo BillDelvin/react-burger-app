93fd4445043185d4b32ba8a15e6ccb442e74f76f
billdelvin <billdelvin6@gmail.com>
2020-02-12 18:18:26 +0700

    adding orderSummary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Aux from "../../../hoc/Aux";
+
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igItem => {
+    return (
+      <li key={igItem}>
+        <span style={{ textTransform: "capitalize" }}>{igItem}</span>:
+        {props.ingredients[igItem]}
+      </li>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>Delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout? </p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
