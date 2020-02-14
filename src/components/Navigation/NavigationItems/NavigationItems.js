cec6087568d3f004eea5ee97c70ba99274382753
billdelvin <billdelvin6@gmail.com>
2020-02-14 13:01:18 +0700

    adding navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = props => <ul className={classes.navigationItems}></ul>;
+
+export default navigationItems;
