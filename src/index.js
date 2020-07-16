74dac8a739cce646d231946298bb80a68f51d920
Bill Delvin <billdelvin6@gmail.com>
2020-07-16 10:58:18 +0700

    adding redux store

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux"
 import thunk from "redux-thunk"
 import burgerBuilderReducer from "./store/reducer/burgerBuilder"
 import orderReducer from "./store/reducer/order"
+import authReducer from "./store/reducer/auth"
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
+  auth: authReducer,
 })
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
