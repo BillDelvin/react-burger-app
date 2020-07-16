caf741467395fb5c7c2d27179fe908cbc898d625
Bill Delvin <billdelvin6@gmail.com>
2020-07-16 18:20:37 +0700

    make all action in one index

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,2 +1,3 @@
 export { addIngredient, removeIngredient, initIngredient } from "./bugerBuilder"
 export { purchaseBurger, purchaseInit, fetchOrder } from "./order"
+export { auth, logout } from "./auth"
