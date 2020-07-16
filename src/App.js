e984556d5961da7033638f401505a9c10e27100d
Bill Delvin <billdelvin6@gmail.com>
2020-07-16 10:57:51 +0700

    adding route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import CheckOut from "./containers/CheckOut/CheckOut";
-import Orders from "./containers/Orders/Orders";
+import React, { Component } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Layout from "./hoc/Layout/Layout"
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
+import CheckOut from "./containers/CheckOut/CheckOut"
+import Orders from "./containers/Orders/Orders"
+import Auth from "./containers/Auth/Auth"
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <div>
           <Layout>
             <Switch>
+              <Route path="/auth" component={Auth} />
               <Route path="/checkout" component={CheckOut} />
               <Route path="/orders" component={Orders} />
               <Route path="/" exact component={BurgerBuilder} />
@@ -19,8 +21,8 @@ class App extends Component {
           </Layout>
         </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
