ca8a23ccab43473ac8e8f2cffe9cfb3940895bb8
Bill Delvin <billdelvin6@gmail.com>
2020-07-16 18:16:17 +0700

    adding logout route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 import CheckOut from "./containers/CheckOut/CheckOut"
 import Orders from "./containers/Orders/Orders"
 import Auth from "./containers/Auth/Auth"
+import Logout from "./containers/Auth/Logout/Logout"
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Layout>
             <Switch>
               <Route path="/auth" component={Auth} />
+              <Route path="/logout" component={Logout} />
               <Route path="/checkout" component={CheckOut} />
               <Route path="/orders" component={Orders} />
               <Route path="/" exact component={BurgerBuilder} />
