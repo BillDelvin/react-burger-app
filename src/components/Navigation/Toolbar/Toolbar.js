1facc5ccf9b2b2c1206f90bc33d92813fce8c5c3
Bill Delvin <billdelvin6@gmail.com>
2020-07-16 20:57:14 +0700

    adding auth in toolbar nav

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,19 +1,19 @@
-import React from "react";
-import classes from "./Toolbar.css";
-import Logo from "../../Logo/Logo";
-import Navigationitems from "../NavigationItems/NavigationItems";
-import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+import React from "react"
+import classes from "./Toolbar.css"
+import Logo from "../../Logo/Logo"
+import Navigationitems from "../NavigationItems/NavigationItems"
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle"
 
-const toolbar = props => {
+const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
       <DrawerToggle clicked={props.drawerToggleClicked} />
       <Logo />
       <nav className={classes.DesktopOnly}>
-        <Navigationitems />
+        <Navigationitems isAuthenticated={props.isAuth} />
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default toolbar;
+export default toolbar
