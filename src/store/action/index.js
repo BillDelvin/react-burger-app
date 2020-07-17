950d6dfad5c5086816545cc0000175fabd5c56bc
Bill Delvin <billdelvin6@gmail.com>
2020-07-17 13:19:54 +0700

    remove href

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredient } from "./bugerBuilder"
 export { purchaseBurger, purchaseInit, fetchOrder } from "./order"
-export { auth, logout, setAuthRedirectPath } from "./auth"
+export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth"
