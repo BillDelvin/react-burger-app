7d1ebb2d6552f8de6aa0f4f0c3806c113386fd8e
Bill Delvin <billdelvin6@gmail.com>
2020-07-18 12:04:38 +0700

    setting production tool

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import registerServiceWorker from "./registerServiceWorker"
-import { Provider } from "react-redux"
-import { createStore, applyMiddleware, compose, combineReducers } from "redux"
-import thunk from "redux-thunk"
-import burgerBuilderReducer from "./store/reducer/burgerBuilder"
-import orderReducer from "./store/reducer/order"
-import authReducer from "./store/reducer/auth"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import burgerBuilderReducer from './store/reducer/burgerBuilder'
+import orderReducer from './store/reducer/order'
+import authReducer from './store/reducer/auth'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
@@ -25,5 +28,5 @@ const app = (
   </Provider>
 )
 
-ReactDOM.render(app, document.getElementById("root"))
+ReactDOM.render(app, document.getElementById('root'))
 registerServiceWorker()
